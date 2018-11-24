d54b1000f0382777caab4b55721af730cdb4b708
Samir Ahmed Ghouri <33974817+samirghouri@users.noreply.github.com>
2018-11-24 15:22:31 +0530

    Update Course.java

--- src/main/java/com/Software/Learning_Track_Relevance/model/Course.java
+++ src/main/java/com/Software/Learning_Track_Relevance/model/Course.java
@@ -1,4 +1,24 @@
-package com.Software.Learning_Track_Relevance.model;
+package main.java.com.Software.learning_track_relevance.model;
 
-public class Course {
+import org.neo4j.ogm.annotation.GraphId;
+import org.neo4j.ogm.annotation.NodeEntity;
+
+@NodeEntity
+public class course {
+
+    @GraphId
+    private Long id;
+    private String name;
+
+    public course() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
