2cb1f78ebd5ee35747bcc11b62d256022cc2cd6c
Soumyadip <Soumyadipghosh220114@gmail.com>
2018-11-24 15:06:06 +0530

    Initial

---
+++ src/test/java/com/Software/Learning_Track_Relevance/LearningTrackRelevanceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.Software.Learning_Track_Relevance;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LearningTrackRelevanceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
