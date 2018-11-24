2679a8b0acde6735bd3662be750aec0b12155d27
Soumyadip <Soumyadipghosh220114@gmail.com>
2018-11-24 15:56:54 +0530

    Slight Changes

--- src/main/java/com/Software/Learning_Track_Relevance/resource/CourseResource.java
+++ src/main/java/com/Software/Learning_Track_Relevance/resource/CourseResource.java
@@ -1,12 +1,21 @@
 package com.Software.Learning_Track_Relevance.resource;
 
+import com.Software.Learning_Track_Relevance.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/rest/neo4j/course")
 public class CourseResource {
 
+    @Autowired
+    CourseService courseService;
+
+    @GetMapping
+    public Collection<Course> getAll();
 
 }
---
+++ src/main/java/com/Software/Learning_Track_Relevance/service/CourseService.java
@@ -0,0 +1,9 @@
+package com.Software.Learning_Track_Relevance.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CourseService {
+
+
+}
